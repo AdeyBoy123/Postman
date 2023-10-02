6f6dbbff7c1bb77657c9c721d8d4d96eec7f9b24
AdeyBoy123 <groveraden@gmail.com>
2023-10-02 10:48:40 +0100

    Add project files.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        /*public string Get()
+        {
+            return "Hello World";
+            
+        }*/
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+        //... api/Home
+        public string[] Get()
+        {
+            return new string[] { "apple", "banana", "orange" };
+        }
+    }
+}
